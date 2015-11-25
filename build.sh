e219b8c084a2dad6f96efdd14d673c4721764c8b
Shunta Saito <shunta.saito@gmail.com>
2015-11-26 06:10:58 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,5 @@
 #! /bin/bash
 
-######### PLEASE MODIFY #########
-
-TBB_INSTALL_DIR=/opt/intel/tbb
-TBB_ARCH=intel64
-TBB_COMPILER=gcc4.4
-
-OPENCV_SHARE_DIR=$HOME/Libraries/opencv/install/share/OpenCV
-OPENCV_CONTRIB_DIR=$HOME/Libraries/opencv_contrib
-
-BOOST_INSTALL_DIR=$HOME/Libraries/boost_1_58_0/install
-BOOST_NUMPY_INSTALL_DIR=$HOME/Libraries/Boost.NumPy/install
-
-#################################
-
 # Build
 if [ ! -d "build" ]; then
   mkdir build
@@ -21,20 +7,15 @@ fi
 
 cd build
 
-export TBB_INSTALL_DIR=$TBB_INSTALL_DIR
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TBB_INSTALL_DIR/lib/$TBB_ARCH/$TBB_COMPILER
-
-cmake \
--DOpenCV_DIR=$OPENCV_SHARE_DIR \
--DBOOST_ROOT=$BOOST_INSTALL_DIR \
--DBoost_NumPy_INCLUDE_DIR=$BOOST_NUMPY_INSTALL_DIR/include \
--DBoost_NumPy_LIBRARY_DIR=$BOOST_NUMPY_INSTALL_DIR/lib \
-../
-
+cmake ../
 make
 touch __init__.py
 
+git clone https://github.com/Itseez/opencv_contrib
+mv opencv_contrib/modules/saliency/samples/ObjectnessTrainedModel ./
+rm -rf opencv_contrib
+
 cd ..
 
-cp -r $OPENCV_CONTRIB_DIR/modules/saliency/samples/ObjectnessTrainedModel build/
 wget http://farm1.static.flickr.com/121/278839518_140821637d.jpg -O sample.jpg
+python scripts/test_bing.py
